408b6f8197206bf94adc4e7a8be02bc9d2506999
Jeswin <jeswinpk@agilehead.com>
2019-12-05 02:21:03 +0530

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,64 @@
+import exception from "./exception";
+
+export interface IndexSpecification {}
+
+export interface LogEntry {
+  source: string;
+  operation: "created" | "deleted";
+  data?: string;
+  commitHash: string;
+  previousHash: string;
+  commitMessage: string;
+  author: string;
+  timestamp: string;
+  branch: string;
+}
+
+export type FileTypes = "data" | "index";
+
+export interface IDbClient {
+  execute(sql: string): Promise<any>;
+}
+
+export async function processLogs(entries: LogEntry[], dbClient: IDbClient) {
+  async function processLog(entry: LogEntry) {
+    return entry.operation === "created"
+      ? processCreation(entry)
+      : entry.operation === "deleted"
+      ? processDeletion(entry)
+      : exception(
+          `Invalid operation ('${entry.operation}') in commit ${entry.commitHash}:${entry.source}.`
+        );
+  }
+
+  /*
+    Delete all records associated with the file.
+  */
+  async function processDeletion(entry: LogEntry) {
+    const [table, file] = getTableAndFile(entry.source);
+    const query = `DELETE * FROM ${table} WHERE gista_file='${file}';`;
+    await dbClient.execute(query);
+  }
+
+  /*
+    New records need to be inserted.
+  */
+  async function processCreation(entry: LogEntry) {
+    
+  }
+
+  for (const entry of entries) {
+    await processLog(entry);
+  }
+}
+
+type LogTarget = [string?, string?];
+
+function getTableAndFile(source: string): LogTarget {
+  const [table, ...file] = source.split("/");
+  return isDataFile(source) ? [table, file.join("/")] : ([] as LogTarget);
+}
+
+function isDataFile(source: string) {
+  return true;
+}
